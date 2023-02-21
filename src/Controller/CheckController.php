98429054d4307ca6b803e5502fec05efdb6267c7
Piotr Zakrzewski <piotrzakrzewski89@gmail.com>
2023-02-21 20:12:51 +0100

    first initail, working project docker build + api platform symfony 6.2.6

---
+++ src/Controller/CheckController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class CheckController extends AbstractController
+{
+    #[Route('/check', name: 'app_check', methods: 'GET')]
+    public function index(): JsonResponse
+    {
+        return $this->json(
+            [
+                'currDate' => new \DateTime(),
+            ]
+        );
+    }
+}
